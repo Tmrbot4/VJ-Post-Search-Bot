d4086e88a4fc8f52a87b564152022d125075de22
Tmrbot4 <166976733+Tmrbot4@users.noreply.github.com>
2024-07-26 20:35:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+API_ID       = int(environ.get("API_ID", "27696177"))
+API_HASH     = environ.get("API_HASH", "0c44906a4feff3b947db76dfa7c57d88")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "7263960107:AAHpuNFgo562xuJLi88WdKQ3GEgtv74B0UI")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://tabolo8539:0evqZDV4fC5fD17c@cluster0.cw8zxus.mongodb.net/?retryWrites=true&w=majority")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002213521354"))
+ADMIN        = int(environ.get("ADMIN", "1256858577"))
+CHANNEL      = environ.get("CHANNEL", "@TMR_update_channel")
